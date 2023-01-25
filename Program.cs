e1c88e4a9bc4e258f5797581b052f0022f58afb2
Popenova <123539986+Popenova@users.noreply.github.com>
2023-01-25 13:01:32 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Введите текст: ");
+Console.WriteLine("Количество слов = {0}", Console.ReadLine().Split(' ').Length);
+Console.WriteLine("Количество пробелов = {0}", Console.ReadLine().Split().Length);
